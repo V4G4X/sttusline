800b4bae4238d7f9e14b93dccb61e8abf28fae5e
Varun Gawande <varun.gawande@espressif.com>
2024-05-13 16:36:31 +0530

    Fix: Incorrect branch in worktrees
    Some people create worktrees inside the original repository.
    
    repo (branch_A)
    |
    -> repo_wt (branch_B)
    
    In this case, even when inside the repo_wt, the branch name function would return branch_A, instead of branch_B.

--- lua/sttusline/components/git-branch.lua
+++ lua/sttusline/components/git-branch.lua
@@ -1,32 +1,28 @@
 local colors = require("sttusline.utils.color")
 
+local function all_trim(s)
+    return s:match("^%s*(.-)%s*$")
+end
+
 return {
-	name = "git-branch",
-	event = "BufEnter",
-	user_event = { "VeryLazy", "GitSignsUpdate" },
-	configs = {
-		icon = "",
-	},
-	colors = { fg = colors.pink },
-	space = {
-		get_branch = function()
-			local git_dir = vim.fn.finddir(".git", ".;")
-			if git_dir ~= "" then
-				local head_file = io.open(git_dir .. "/HEAD", "r")
-				if head_file then
-					local content = head_file:read("*all")
-					head_file:close()
-					-- branch name  or commit hash
-					return content:match("ref: refs/heads/(.-)%s*$") or content:sub(1, 7) or ""
-				end
-				return ""
-			end
-			return ""
-		end,
-	},
-	update = function(configs, space)
-		local branch = space.get_branch()
-		return branch ~= "" and configs.icon .. " " .. branch or ""
-	end,
-	condition = function() return vim.api.nvim_buf_get_option(0, "buflisted") end,
+    name = "git-branch",
+    event = "BufEnter",
+    user_event = { "VeryLazy", "GitSignsUpdate" },
+    configs = {
+        icon = "",
+    },
+    colors = { fg = colors.pink },
+    space = {
+        get_branch = function()
+            local file = io.popen('git branch --show-current 2> /dev/null')
+            local output = file:read('*a')
+            file:close()
+            return all_trim(output)
+        end,
+    },
+    update = function(configs, space)
+        local branch = space.get_branch()
+        return branch ~= "" and configs.icon .. " " .. branch or ""
+    end,
+    condition = function() return vim.api.nvim_buf_get_option(0, "buflisted") end,
 }
